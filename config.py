7ab949de95c1cfdd4bf45d9d7d6b7412db5e814e
jayasrikeerthi <159767501+jayasrikeerthi@users.noreply.github.com>
2025-02-24 17:34:48 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://ADMIN\\SQLEXPRESS/account_management?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
